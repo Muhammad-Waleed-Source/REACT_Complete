ee01b2e76e7e09a7e1856d1b5b3597a32ba55f76
Muhammad Waleed <mwaleedkhan124@gmail.com>
2024-12-16 13:09:52 +0500

    16-12-2024

--- Projects/6-todo-app-version-three/src/components/AddTodo.jsx
+++ Projects/6-todo-app-version-three/src/components/AddTodo.jsx
@@ -1,47 +1,43 @@
 import { IoMdAddCircleOutline } from "react-icons/io";
 import styles from "./TodoItems.module.css";
-import { useState } from "react";
+import { useRef } from "react";
 
 function AddTodo({ onNewItem }) {
-  const [todoName, setTodoName] = useState('');
-  const [dueDate, setDueDate] = useState('');
 
-  const handleNameChange = (event) => {
-    setTodoName(event.target.value);
-  };
-  const handleDateChange = (event) => {
-    setDueDate(event.target.value);
-  };
-  const handleAddButtonClicked = () => {
+  const todoNameElement = useRef();
+  const dueDateElement = useRef();
+
+  const handleAddButtonClicked = (event) => {
+    event.preventDefault();
+    const todoName = todoNameElement.current.value;
+    const dueDate = dueDateElement.current.value;
+    todoNameElement.current.value = "";
+    dueDateElement.current.value = "";
     onNewItem(todoName, dueDate);
-    setTodoName("");
-    setDueDate("");
-  }
+  };
 
   return (
     <div className="container">
-      <div className="row kg-row">
+      <form className="row kg-row" onSubmit={handleAddButtonClicked}>
         <div className="col-6">
           <input
             type="text"
+            ref={todoNameElement}
             placeholder="Enter Todo Here"
-            onChange={handleNameChange}
-            value={todoName}
           />
         </div>
         <div className="col-4">
-          <input type="date" onChange={handleDateChange} value={dueDate} />
+          <input
+            type="date"
+            ref={dueDateElement}
+          />
         </div>
         <div className="col-2">
-          <button
-            type="button"
-            className="btn btn-success kg-button"
-            onClick={handleAddButtonClicked}
-          >
+          <button className="btn btn-success kg-button">
             <IoMdAddCircleOutline className={styles.addButton} />
           </button>
         </div>
-      </div>
+      </form>
     </div>
   );
 }
