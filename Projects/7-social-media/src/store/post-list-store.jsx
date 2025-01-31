66bc7d26925480c3253bfefb154d939784180403
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-01-31 16:14:31 +0500

    31-01-2025

---
+++ Projects/7-social-media/src/store/post-list-store.jsx
@@ -0,0 +1,61 @@
+import { createContext, useReducer } from "react";
+
+export const PostList = createContext({
+  postList: [],
+  addPost: () => {},
+  deletePost: () => {},
+});
+
+const postListReducer = (currPostList, action) => {
+  let newPostList = currPostList;
+  if (action.type === "DELETE_POST") {
+    newPostList = currPostList.filter(
+      (post) => post.id !== action.payload.postId
+    );
+  }
+  return newPostList;
+};
+
+const PostListProvider = ({ children }) => {
+  const [postList, dispatchPostList] = useReducer(
+    postListReducer,
+    DEFAULT_POST_LIST
+  );
+
+  const addPost = () => {};
+  const deletePost = (postId) => {
+    dispatchPostList({
+      type: "DELETE_POST",
+      payload: {
+        postId,
+      },
+    });
+  };
+
+  return (
+    <PostList.Provider value={{ postList, addPost, deletePost }}>
+      {children}
+    </PostList.Provider>
+  );
+};
+
+const DEFAULT_POST_LIST = [
+  {
+    id: "1",
+    title: "Going to Lahore",
+    body: "Hi friends I am goint to Lahore for my vacations. Hope to enjoy alot",
+    reactions: 2,
+    userId: "user-9",
+    tags: ["vacations", "Lahore", "Enjoying"],
+  },
+  {
+    id: "2",
+    title: "Working on Project",
+    body: "After a long period of time I started working on the project that we were working on since 2020.",
+    reactions: 15,
+    userId: "user-12",
+    tags: ["Project", "Stress", "Enjoying"],
+  },
+];
+
+export default PostListProvider;
