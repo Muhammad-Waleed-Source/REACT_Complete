1bb4082068f7d72462dd6ea562ebb0cd9241aecf
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-02-10 12:56:19 +0500

    10/02/2025

---
+++ 46 react-router/src/components/CreatePost.jsx
@@ -0,0 +1,121 @@
+import React, { useContext, useRef } from "react";
+import { PostList } from "../store/post-list-store";
+
+function CreatePost() {
+  const { addPost } = useContext(PostList);
+  const userIdElement = useRef();
+  const postTitleElement = useRef();
+  const postBodyElement = useRef();
+  const reactionsElement = useRef();
+  const tagsElement = useRef();
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    const userId = userIdElement.current.value;
+    const postTitle = postTitleElement.current.value;
+    const postBody = postBodyElement.current.value;
+    const reactions = reactionsElement.current.value;
+    const tags = tagsElement.current.value.split(" ");
+
+    /*userIdElement.current.value = "";
+    postTitleElement.current.value = "";
+    postBodyElement.current.value = "";
+    reactionsElement.current.value = "";
+    tagsElement.current.value = "";*/
+
+    console.log("Sending post to server")
+    fetch("https://dummyjson.com/posts/add", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({
+        title: postTitle,
+        body: postBody,
+        reactions: reactions,
+        userId: userId,
+        tags: tags,
+      }),
+    })
+      .then((res) => res.json())
+      .then((post) => {
+        console.log("Got response from server", post)
+        addPost(post);
+      });
+  };
+
+  return (
+    <form className="create-post" onSubmit={handleSubmit}>
+      <div className="mb-3">
+        <label htmlFor="userId" className="form-label">
+          Enter your user id here
+        </label>
+        <input
+          type="text"
+          ref={userIdElement}
+          rows={4}
+          className="form-control"
+          id="userId"
+          placeholder="Your User Id"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="title" className="form-label">
+          Post Title
+        </label>
+        <input
+          type="text"
+          ref={postTitleElement}
+          className="form-control"
+          id="title"
+          placeholder="How are you feeling today..."
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="body" className="form-label">
+          Post Content
+        </label>
+        <textarea
+          type="text"
+          ref={postBodyElement}
+          rows={4}
+          className="form-control"
+          id="body"
+          placeholder="Tell us more about it"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="reactions" className="form-label">
+          Number of reactions
+        </label>
+        <input
+          ref={reactionsElement}
+          type="text"
+          className="form-control"
+          id="reactions"
+          placeholder="How many people reacted to this post"
+        />
+      </div>
+
+      <div className="mb-3">
+        <label htmlFor="tags" className="form-label">
+          Enter you hashtags here
+        </label>
+        <input
+          ref={tagsElement}
+          type="text"
+          className="form-control"
+          id="tags"
+          placeholder="Please enter tags using space"
+        />
+      </div>
+
+      <button type="submit" className="btn btn-primary">
+        Post
+      </button>
+    </form>
+  );
+}
+
+export default CreatePost;
