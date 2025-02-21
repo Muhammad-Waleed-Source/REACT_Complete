498b1d9ce2cf1e57ecb6c859243766491720b88c
Muhammad Waleed <mwaleedkhan124@gmail.com>
2025-02-21 16:50:19 +0500

    21/02/2025

--- Projects/9-Myntra Clone/3-myntra-react-clone/src/main.jsx
+++ Projects/9-Myntra Clone/3-myntra-react-clone/src/main.jsx
@@ -1,10 +1,32 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import { createRoot } from "react-dom/client";
+import "./index.css";
+import App from "./routes/App.jsx";
+import Bag from "./routes/Bag.jsx";
+import Home from "./routes/Home.jsx";
 
-createRoot(document.getElementById('root')).render(
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <App />,
+    children: [
+      {
+        path: "/",
+        element: <Home />
+      },
+      {
+        path: "/bag",
+        element: <Bag />,
+      },
+    ],
+  },
+]);
+
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <RouterProvider router={router}>
+      <App />
+    </RouterProvider>
+  </StrictMode>
+);
